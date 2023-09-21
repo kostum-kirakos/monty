57af8c1e53d9c59fc45bff063595401d33f41fa0
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-09-21 22:03:32 +0100

    betty

--- main.c
+++ main.c
@@ -36,55 +36,68 @@ int main(int argc, char *argv[])
 	instruction_t *command;
 	int find;
 
-
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		return (1);
-	}
-
+	check_args_error(argc);
 	file_to_open = fopen(argv[1], "r");
-
-	if (!file_to_open)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		return (1);
-	}
-
+	check_openfile_erro(file_to_open, argv[1]);
 	while (fgets(line, sizeof(line), file_to_open) != NULL)
 	{
 		line_number++;
 		opcode = strtok(line, " \t\n");
-
 		if (opcode == NULL || opcode[0] == '#')
 			continue; /* Skip empty lines and comments */
-
 		find = 0;
-
 		for (command = instructions; command->opcode != NULL; command++)
 		{
 			if (strcmp(opcode, command->opcode) == 0)
 			{
 				command->f(&stack, line_number);
 				find = 1;
-				break;
-			}
+				break; }
 		}
 		if (!find)
 		{
 			fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
 			fclose(file_to_open);
-			return (1);
-		}
+			return (1); }
 	}
-
 	fclose(file_to_open);
 	while (stack != NULL)
 	{
 		stack_t *temp = stack;
 
 		stack = stack->next;
-		free(temp);
-	}
+		free(temp); }
 	return (0);
+}
+
+
+/**
+ * check_args_error - Check if the number of arguments is correct.
+ * @argc: Number of arguments.
+ * Return: Nothing.
+ */
+void check_args_error(int argc)
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+}
+
+
+/**
+ * check_openfile_erro - Check if the file can be opened.
+ * @file_to_open: File to open.
+ * @argv: Array of argument strings.
+ * Return: Nothing.
+ */
+
+void check_openfile_erro(FILE *file_to_open, char *argv)
+{
+	if (!file_to_open)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv);
+		exit(EXIT_FAILURE);
+	}
 }
