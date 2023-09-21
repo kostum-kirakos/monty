d37d384fcf094432bcbed72878e0c87f3c435bc1
kostum-kirakos <lkoustim.mohamed@yahoo.com>
2023-09-21 12:31:59 -0700

    fix mul

--- mul.c
+++ mul.c
@@ -10,17 +10,17 @@
 
 void mul(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp = NULL;
+	stack_t *temp;
 
-	if (!stack || !*stack || !(*stack)->next)
+	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
 	temp = *stack;
-	(*stack)->next->n *= (*stack)->n;
 	*stack = (*stack)->next;
+	(*stack)->n *= temp->n;
 	(*stack)->prev = NULL;
 	free(temp);
 }
