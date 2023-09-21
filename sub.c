71189b8a90a715670adde436c3b2bfd9929e398a
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-09-21 18:48:06 +0100

    sub function

--- sub.c
+++ sub.c
@@ -1,18 +1,18 @@
 #include "monty.h"
 
 /**
- * add - Adds the top two elements of the stack.
+ * sub - Substract the top two elements of the stack.
  * @stack: Pointer to the stack.
  * @line_number: Line number in the Monty bytecode file.
  */
 
-void add(stack_t **stack, unsigned int line_number)
+void sub(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp;
 
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-	fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+	fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
 	exit(EXIT_FAILURE);
 	}
 
