b3d478e0b6b3d618e6d0578c17c4090562441598
kostum-kirakos <lkoustim.mohamed@yahoo.com>
2023-09-20 19:04:15 -0700

    pint func

--- pint.c
+++ pint.c
@@ -1,21 +1,28 @@
 #include "monty.h"
 
 /**
- * pint - prints the value at the top of the stack
-* @stack: pointer to the stack
-* @line_number: linr number
-* Return: void
-*/
-
+ * pint - Print the value at the top of the stack
+ * @stack: Pointer to the stack
+ * @line_number: Line number in the file
+ */
 void pint(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp = *stack;
+    if (*stack == NULL)
+    {
+        fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+    printf("%d\n", (*stack)->n);
+}
+void free_all(stack_t **stack)
+{
+    stack_t *current = *stack;
+    stack_t *temp;
 
-	if (temp == NULL)
-	{
-		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-
-	printf("%d\n", temp->n);
+    while (current != NULL)
+    {
+        temp = current;
+        current = current->next;
+        free(temp);
+    }
 }
