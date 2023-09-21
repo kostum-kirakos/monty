43f2ba921e972b2277ec6f549f79eb462756f606
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-09-21 18:48:11 +0100

    adding sub to header

--- monty.h
+++ monty.h
@@ -47,6 +47,6 @@ void add(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 int isnumber(char *s);
 void nop(stack_t **stack, unsigned int line_number);
-void add(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
 
 #endif /* MONTY_H */
