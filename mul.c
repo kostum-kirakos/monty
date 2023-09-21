276e59f1b6aec00b070aba6d84d340cfd45e5225
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-09-21 20:29:35 +0100

    task 8

--- mul.c
+++ mul.c
@@ -12,15 +12,15 @@ void mul(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp = NULL;
 
-	if (*stack == NULL || (*stack)->next == NULL)
+	if (!stack || !*stack || !(*stack)->next)
 	{
-		fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
 	temp = *stack;
+	(*stack)->next->n *= (*stack)->n;
 	*stack = (*stack)->next;
-	(*stack)->n *= temp->n;
 	(*stack)->prev = NULL;
 	free(temp);
 }
