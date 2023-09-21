7e3ac5c405df67195bdbcabd13ca76d692328ae7
kostum-kirakos <lkoustim.mohamed@yahoo.com>
2023-09-21 13:43:39 -0700

    im done

--- rotr.c
+++ rotr.c
@@ -10,19 +10,19 @@
 
 void rotr(stack_t **stack, unsigned int line_number)
 {
-    stack_t *temp = *stack;
+	stack_t *temp = *stack;
 
-    (void)line_number;
-    if (*stack == NULL || (*stack)->next == NULL)
-        return;
+	(void)line_number;
+	if (*stack == NULL || (*stack)->next == NULL)
+		return;
 
-    while (temp->next != NULL)
-    {
-        temp = temp->next;
-    }
-    temp->next = *stack;
-    (*stack)->prev = temp;
-    *stack = (*stack)->prev;
-    (*stack)->prev->next = NULL;
-    (*stack)->prev = NULL;
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	temp->next = *stack;
+	(*stack)->prev = temp;
+	*stack = (*stack)->prev;
+	(*stack)->prev->next = NULL;
+	(*stack)->prev = NULL;
 }
