bda57dd8624aeb6b9081123b69baa117650a0986
kostum-kirakos <lkoustim.mohamed@yahoo.com>
2023-09-21 09:36:47 -0700

    fixing pop hop last fix

--- pop.c
+++ pop.c
@@ -10,12 +10,6 @@ void pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp;
 
-	if (*stack == NULL)
-	{
-		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-
 	temp = *stack;
 	*stack = (*stack)->next;
 
@@ -23,5 +17,11 @@ void pop(stack_t **stack, unsigned int line_number)
 	{
 		(*stack)->prev = NULL;
 		free(temp);
+	}
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
 	}
 }
